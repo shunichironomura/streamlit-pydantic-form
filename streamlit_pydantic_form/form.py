f4b80d24940614891a1fb148ad817ddf7771abe0
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-02-16 23:31:01 +0900

    Update form and widget builders, add containerize class

--- streamlit_pydantic_form/form.py
+++ streamlit_pydantic_form/form.py
@@ -1,11 +1,14 @@
 import warnings
+from collections.abc import Callable
+from functools import wraps
 from inspect import isclass
 from types import GenericAlias, TracebackType
-from typing import Any, Generic, Self, TypeVar, get_args, get_origin
+from typing import Any, Generic, ParamSpec, Self, TypeVar, get_args, get_origin
 
 import streamlit as st
 from pydantic import BaseModel
 from pydantic_core import PydanticUndefined
+from streamlit.delta_generator import DeltaGenerator
 from typing_extensions import deprecated
 
 from .widget import WidgetBuilder
@@ -32,8 +35,8 @@ class st_auto_form(Generic[_T]):  # noqa: N801
 
     def input_widgets(self) -> _T:
         if self.widget_builder is not None:
-            return self.widget_builder.build()
-        return _model_to_input_components(self.model)
+            return self.widget_builder.build(self.form)
+        return _model_to_input_components(self.model, form=self.form)
 
     @deprecated(
         "st_auto_form.input_components() is deprecated, use st_auto_form.input_widgets() instead",
@@ -45,6 +48,10 @@ class st_auto_form(Generic[_T]):  # noqa: N801
             stacklevel=2,
         )
         return self.input_widgets()
+
+    @property
+    def form_submit_button(self) -> Callable[..., bool]:
+        return self.form.form_submit_button
 
     def __enter__(self) -> Self:
         # Enter the inner st.form
@@ -75,15 +82,38 @@ def _extract_widget_builder_from_metadata(metadata: list[Any]) -> WidgetBuilder[
 
 _SUPPORTED_GENERIC_ALIAS = {list}
 
+_P = ParamSpec("_P")
+_R = TypeVar("_R")
 
-def _model_to_input_components(model: type[_T]) -> _T:
+
+class containerize:
+    def __init__(self, *, height: int | None = None, border: bool | None = None) -> None:
+        self.height = height
+        self.border = border
+
+    def __call__(self, func: Callable[_P, _R]) -> Callable[_P, _R]:
+        @wraps(func)
+        def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _R:
+            with st.container(height=self.height, border=self.border):
+                return func(*args, **kwargs)
+
+        return wrapper
+
+
+def _model_to_input_components(
+    model: type[_T],
+    *,
+    form: DeltaGenerator,
+    randomize_key: bool = False,
+) -> _T:
     raw_input_values = {}
     for name, field in model.model_fields.items():
         try:
             builder = _extract_widget_builder_from_metadata(field.metadata)
             if field.default is not PydanticUndefined:
                 builder.default = field.default
-            raw_input_values[name] = builder.build()
+            raw_input_values[name] = builder.build(form, randomize_key=randomize_key)
+
         except NoWidgetBuilderFoundError:
             if field.annotation is None:
                 raise
@@ -91,20 +121,28 @@ def _model_to_input_components(model: type[_T]) -> _T:
                 isinstance(field.annotation, GenericAlias)
                 and (origin := get_origin(field.annotation)) in _SUPPORTED_GENERIC_ALIAS
             ):
-                st.write(f"{get_args(field.annotation)=}")
                 if origin is list:
                     with st.container(border=True):
-                        raw_input_values[name] = _models_list_to_input_components(get_args(field.annotation)[0])
+                        raw_input_values[name] = _models_list_to_input_components(
+                            get_args(field.annotation)[0],
+                            form=form,
+                        )
             elif isclass(field.annotation) and issubclass(field.annotation, BaseModel):
                 with st.container(border=True):
-                    raw_input_values[name] = _model_to_input_components(field.annotation)
+                    raw_input_values[name] = _model_to_input_components(
+                        field.annotation,
+                        form=form,
+                        randomize_key=randomize_key,
+                    )
             else:
                 raise
 
     return model(**raw_input_values)
 
 
-def _models_list_to_input_components(models: type[_T]) -> list[_T]:
-    n_items = st.number_input("Number of items", min_value=1, value=1)
-    st.write(f"{n_items=}")
-    return [_model_to_input_components(models) for _ in range(n_items)]
+def _models_list_to_input_components(model: type[_T], *, form: DeltaGenerator) -> list[_T]:
+    n_items = int(st.number_input(f"Number of `{model.__name__}` items", min_value=1, value=1))
+    return [
+        containerize(border=True)(_model_to_input_components)(model, form=form, randomize_key=True)
+        for _ in range(n_items)
+    ]
