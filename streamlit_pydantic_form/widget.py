1709bd159cb2648ca7b32ee64dfea4e0150796ca
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-05-23 22:18:04 +0900

    add `kwargs` to `build` method

--- streamlit_pydantic_form/widget.py
+++ streamlit_pydantic_form/widget.py
@@ -28,6 +28,7 @@ class WidgetBuilder(ABC, Generic[_T]):
         *,
         randomize_key: bool = False,
         value: _T | object = _NOT_SET,
+        kwargs: Any = _NOT_SET,
     ) -> _T: ...
 
 
@@ -42,8 +43,8 @@ def _generate_random_key() -> str:
 
 class Checkbox(WidgetBuilder[bool]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
+        self._args = args
+        self._kwargs = kwargs
         self.default: bool = False
 
     def build(
@@ -52,18 +53,20 @@ class Checkbox(WidgetBuilder[bool]):
         *,
         randomize_key: bool = False,
         value: _T | object = _NOT_SET,
+        kwargs: Any = _NOT_SET,
     ) -> bool:
+        kwargs = self._kwargs | kwargs if kwargs is not _NOT_SET else self._kwargs
         value = value if value is not _NOT_SET else self.default
-        kwargs = self.kwargs | {"value": value}
+        kwargs = kwargs | {"value": value}
         if randomize_key:
             kwargs["key"] = _generate_random_key()
-        return st.checkbox(*self.args, **kwargs) if form is None else form.checkbox(*self.args, **kwargs)
+        return st.checkbox(*self._args, **kwargs) if form is None else form.checkbox(*self._args, **kwargs)
 
 
 class Toggle(WidgetBuilder[bool]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
+        self._args = args
+        self._kwargs = kwargs
         self.default: bool = False
 
     def build(
@@ -72,18 +75,20 @@ class Toggle(WidgetBuilder[bool]):
         *,
         randomize_key: bool = False,
         value: _T | object = _NOT_SET,
+        kwargs: Any = _NOT_SET,
     ) -> bool:
+        kwargs = self._kwargs | kwargs if kwargs is not _NOT_SET else self._kwargs
         value = value if value is not _NOT_SET else self.default
-        kwargs = self.kwargs | {"value": value}
+        kwargs = kwargs | {"value": value}
         if randomize_key:
             kwargs["key"] = _generate_random_key()
-        return st.toggle(*self.args, **kwargs) if form is None else form.toggle(*self.args, **kwargs)
+        return st.toggle(*self._args, **kwargs) if form is None else form.toggle(*self._args, **kwargs)
 
 
 class Radio(WidgetBuilder[Any | None]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
+        self._args = args
+        self._kwargs = kwargs
         self.default: int | None = 0
 
     def build(
@@ -92,18 +97,20 @@ class Radio(WidgetBuilder[Any | None]):
         *,
         randomize_key: bool = False,
         value: _T | object = _NOT_SET,
+        kwargs: Any = _NOT_SET,
     ) -> Any | None:
+        kwargs = self._kwargs | kwargs if kwargs is not _NOT_SET else self._kwargs
         value = value if value is not _NOT_SET else self.default
-        kwargs = self.kwargs | {"index": value}
+        kwargs = kwargs | {"index": value}
         if randomize_key:
             kwargs["key"] = _generate_random_key()
-        return st.radio(*self.args, **kwargs) if form is None else form.radio(*self.args, **kwargs)
+        return st.radio(*self._args, **kwargs) if form is None else form.radio(*self._args, **kwargs)
 
 
 class Selectbox(WidgetBuilder[Any | None]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
+        self._args = args
+        self._kwargs = kwargs
         self.default: int | None = 0
 
     def build(
@@ -112,18 +119,20 @@ class Selectbox(WidgetBuilder[Any | None]):
         *,
         randomize_key: bool = False,
         value: _T | object = _NOT_SET,
+        kwargs: Any = _NOT_SET,
     ) -> Any | None:
+        kwargs = self._kwargs | kwargs if kwargs is not _NOT_SET else self._kwargs
         value = value if value is not _NOT_SET else self.default
-        kwargs = self.kwargs | {"index": value}
+        kwargs = kwargs | {"index": value}
         if randomize_key:
             kwargs["key"] = _generate_random_key()
-        return st.selectbox(*self.args, **kwargs) if form is None else form.selectbox(*self.args, **kwargs)
+        return st.selectbox(*self._args, **kwargs) if form is None else form.selectbox(*self._args, **kwargs)
 
 
 class Multiselect(WidgetBuilder[list[Any]]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
+        self._args = args
+        self._kwargs = kwargs
         self.default: Any | None = None
 
     def build(
@@ -132,18 +141,20 @@ class Multiselect(WidgetBuilder[list[Any]]):
         *,
         randomize_key: bool = False,
         value: _T | object = _NOT_SET,
+        kwargs: Any = _NOT_SET,
     ) -> list[Any]:
+        kwargs = self._kwargs | kwargs if kwargs is not _NOT_SET else self._kwargs
         value = value if value is not _NOT_SET else self.default
-        kwargs = self.kwargs | {"default": value}
+        kwargs = kwargs | {"default": value}
         if randomize_key:
             kwargs["key"] = _generate_random_key()
-        return st.multiselect(*self.args, **kwargs) if form is None else form.multiselect(*self.args, **kwargs)
+        return st.multiselect(*self._args, **kwargs) if form is None else form.multiselect(*self._args, **kwargs)
 
 
 class Slider(WidgetBuilder[Any]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
+        self._args = args
+        self._kwargs = kwargs
         self.default: Any | None = None
 
     def build(
@@ -152,18 +163,20 @@ class Slider(WidgetBuilder[Any]):
         *,
         randomize_key: bool = False,
         value: _T | object = _NOT_SET,
+        kwargs: Any = _NOT_SET,
     ) -> Any:
+        kwargs = self._kwargs | kwargs if kwargs is not _NOT_SET else self._kwargs
         value = value if value is not _NOT_SET else self.default
-        kwargs = self.kwargs | {"value": value}
+        kwargs = kwargs | {"value": value}
         if randomize_key:
             kwargs["key"] = _generate_random_key()
-        return st.slider(*self.args, **kwargs) if form is None else form.slider(*self.args, **kwargs)
+        return st.slider(*self._args, **kwargs) if form is None else form.slider(*self._args, **kwargs)
 
 
 class SelectSlider(WidgetBuilder[Any | tuple[Any]]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
+        self._args = args
+        self._kwargs = kwargs
         self.default = None
 
     def build(
@@ -172,18 +185,20 @@ class SelectSlider(WidgetBuilder[Any | tuple[Any]]):
         *,
         randomize_key: bool = False,
         value: _T | object = _NOT_SET,
+        kwargs: Any = _NOT_SET,
     ) -> Any | tuple[Any]:
+        kwargs = self._kwargs | kwargs if kwargs is not _NOT_SET else self._kwargs
         value = value if value is not _NOT_SET else self.default
-        kwargs = self.kwargs | {"value": value}
+        kwargs = kwargs | {"value": value}
         if randomize_key:
             kwargs["key"] = _generate_random_key()
-        return st.select_slider(*self.args, **kwargs) if form is None else form.select_slider(*self.args, **kwargs)
+        return st.select_slider(*self._args, **kwargs) if form is None else form.select_slider(*self._args, **kwargs)
 
 
 class TextInput(WidgetBuilder[str | None]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
+        self._args = args
+        self._kwargs = kwargs
         self.default: str = ""
 
     def build(
@@ -192,18 +207,20 @@ class TextInput(WidgetBuilder[str | None]):
         *,
         randomize_key: bool = False,
         value: _T | object = _NOT_SET,
+        kwargs: Any = _NOT_SET,
     ) -> str | None:
+        kwargs = self._kwargs | kwargs if kwargs is not _NOT_SET else self._kwargs
         value = value if value is not _NOT_SET else self.default
-        kwargs = self.kwargs | {"value": value}
+        kwargs = kwargs | {"value": value}
         if randomize_key:
             kwargs["key"] = _generate_random_key()
-        return st.text_input(*self.args, **kwargs) if form is None else form.text_input(*self.args, **kwargs)  # type: ignore[no-any-return]
+        return st.text_input(*self._args, **kwargs) if form is None else form.text_input(*self._args, **kwargs)  # type: ignore[no-any-return]
 
 
 class NumberInput(WidgetBuilder[int | float | None]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
+        self._args = args
+        self._kwargs = kwargs
         self.default: int | float | Literal["min"] = "min"
 
     def build(
@@ -212,18 +229,20 @@ class NumberInput(WidgetBuilder[int | float | None]):
         *,
         randomize_key: bool = False,
         value: _T | object = _NOT_SET,
+        kwargs: Any = _NOT_SET,
     ) -> int | float | None:
+        kwargs = self._kwargs | kwargs if kwargs is not _NOT_SET else self._kwargs
         value = value if value is not _NOT_SET else self.default
-        kwargs = self.kwargs | {"value": value}
+        kwargs = kwargs | {"value": value}
         if randomize_key:
             kwargs["key"] = _generate_random_key()
-        return st.number_input(*self.args, **kwargs) if form is None else form.number_input(*self.args, **kwargs)  # type: ignore[no-any-return]
+        return st.number_input(*self._args, **kwargs) if form is None else form.number_input(*self._args, **kwargs)  # type: ignore[no-any-return]
 
 
 class TextArea(WidgetBuilder[str | None]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
+        self._args = args
+        self._kwargs = kwargs
         self.default: str = ""
 
     def build(
@@ -232,18 +251,20 @@ class TextArea(WidgetBuilder[str | None]):
         *,
         randomize_key: bool = False,
         value: _T | object = _NOT_SET,
+        kwargs: Any = _NOT_SET,
     ) -> str | None:
+        kwargs = self._kwargs | kwargs if kwargs is not _NOT_SET else self._kwargs
         value = value if value is not _NOT_SET else self.default
-        kwargs = self.kwargs | {"value": value}
+        kwargs = kwargs | {"value": value}
         if randomize_key:
             kwargs["key"] = _generate_random_key()
-        return st.text_area(*self.args, **kwargs) if form is None else form.text_area(*self.args, **kwargs)  # type: ignore[no-any-return]
+        return st.text_area(*self._args, **kwargs) if form is None else form.text_area(*self._args, **kwargs)  # type: ignore[no-any-return]
 
 
 class DateInput(WidgetBuilder[DateWidgetReturn]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
+        self._args = args
+        self._kwargs = kwargs
         self.default: DateValue | Literal["today"] = "today"
 
     def build(
@@ -252,18 +273,20 @@ class DateInput(WidgetBuilder[DateWidgetReturn]):
         *,
         randomize_key: bool = False,
         value: _T | object = _NOT_SET,
+        kwargs: Any = _NOT_SET,
     ) -> DateWidgetReturn:
+        kwargs = self._kwargs | kwargs if kwargs is not _NOT_SET else self._kwargs
         value = value if value is not _NOT_SET else self.default
-        kwargs = self.kwargs | {"value": value}
+        kwargs = kwargs | {"value": value}
         if randomize_key:
             kwargs["key"] = _generate_random_key()
-        return st.date_input(*self.args, **kwargs) if form is None else form.date_input(*self.args, **kwargs)
+        return st.date_input(*self._args, **kwargs) if form is None else form.date_input(*self._args, **kwargs)
 
 
 class TimeInput(WidgetBuilder[time | None]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
+        self._args = args
+        self._kwargs = kwargs
         self.default: time | datetime | Literal["now"] = "now"
 
     def build(
@@ -272,18 +295,20 @@ class TimeInput(WidgetBuilder[time | None]):
         *,
         randomize_key: bool = False,
         value: _T | object = _NOT_SET,
+        kwargs: Any = _NOT_SET,
     ) -> time | None:
+        kwargs = self._kwargs | kwargs if kwargs is not _NOT_SET else self._kwargs
         value = value if value is not _NOT_SET else self.default
-        kwargs = self.kwargs | {"value": value}
+        kwargs = kwargs | {"value": value}
         if randomize_key:
             kwargs["key"] = _generate_random_key()
-        return st.time_input(*self.args, **kwargs) if form is None else form.time_input(*self.args, **kwargs)  # type: ignore[no-any-return]
+        return st.time_input(*self._args, **kwargs) if form is None else form.time_input(*self._args, **kwargs)  # type: ignore[no-any-return]
 
 
 class FileUploader(WidgetBuilder[Any]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
+        self._args = args
+        self._kwargs = kwargs
 
     def build(
         self,
@@ -291,17 +316,18 @@ class FileUploader(WidgetBuilder[Any]):
         *,
         randomize_key: bool = False,
         value: _T | object = _NOT_SET,  # noqa: ARG002
+        kwargs: Any = _NOT_SET,
     ) -> Any:
-        kwargs = self.kwargs.copy()
+        kwargs = self._kwargs | kwargs if kwargs is not _NOT_SET else self._kwargs
         if randomize_key:
             kwargs["key"] = _generate_random_key()
-        return st.file_uploader(*self.args, **kwargs) if form is None else form.file_uploader(*self.args, **kwargs)
+        return st.file_uploader(*self._args, **kwargs) if form is None else form.file_uploader(*self._args, **kwargs)
 
 
 class CameraInput(WidgetBuilder[UploadedFile | None]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
+        self._args = args
+        self._kwargs = kwargs
 
     def build(
         self,
@@ -309,17 +335,18 @@ class CameraInput(WidgetBuilder[UploadedFile | None]):
         *,
         randomize_key: bool = False,
         value: _T | object = _NOT_SET,  # noqa: ARG002
+        kwargs: Any = _NOT_SET,
     ) -> UploadedFile | None:
-        kwargs = self.kwargs.copy()
+        kwargs = self._kwargs | kwargs if kwargs is not _NOT_SET else self._kwargs
         if randomize_key:
             kwargs["key"] = _generate_random_key()
-        return st.camera_input(*self.args, **kwargs) if form is None else form.camera_input(*self.args, **kwargs)
+        return st.camera_input(*self._args, **kwargs) if form is None else form.camera_input(*self._args, **kwargs)
 
 
 class ColorPicker(WidgetBuilder[str]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.args = args
-        self.kwargs = kwargs
+        self._args = args
+        self._kwargs = kwargs
         self.default: str | None = None
 
     def build(
@@ -328,9 +355,11 @@ class ColorPicker(WidgetBuilder[str]):
         *,
         randomize_key: bool = False,
         value: _T | object = _NOT_SET,
+        kwargs: Any = _NOT_SET,
     ) -> str:
+        kwargs = self._kwargs | kwargs if kwargs is not _NOT_SET else self._kwargs
         value = value if value is not _NOT_SET else self.default
-        kwargs = self.kwargs | {"value": value}
+        kwargs = kwargs | {"value": value}
         if randomize_key:
             kwargs["key"] = _generate_random_key()
-        return st.color_picker(*self.args, **kwargs) if form is None else form.color_picker(*self.args, **kwargs)
+        return st.color_picker(*self._args, **kwargs) if form is None else form.color_picker(*self._args, **kwargs)
